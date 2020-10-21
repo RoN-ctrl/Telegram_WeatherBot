032e09139dc20c9e4120263fad8354f8921accfe
Roman Buzhovych <67021981+RoN-ctrl@users.noreply.github.com>
2020-10-21 17:48:19 +0300

    Added weather response in Bot.java

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -4,8 +4,11 @@ import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import service.impl.WeatherServiceImpl;
 
 public class Bot extends TelegramLongPollingBot {
+    private final WeatherServiceImpl service = new WeatherServiceImpl();
+
     @Override
     public void onUpdateReceived(Update update) {
         var chatId = update.getMessage().getChatId().toString();
@@ -15,7 +18,9 @@ public class Bot extends TelegramLongPollingBot {
         System.out.println(from);
         System.out.println(text);
 
-        sendMessage(chatId, text);
+        var weatherInfo = service.getByCityName(text).toString();
+
+        sendMessage(chatId, weatherInfo);
     }
 
     @SneakyThrows
